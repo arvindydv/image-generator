8bdaef1e4b0c0ac936336bc1d4f3b55d73be78af
arvindydv <arvindydv03@gmail.com>
2024-03-22 20:57:47 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imgUrl, setImgUrl] = useState("");
+  const [query, setQuery] = useState("");
+
+  const imageHandler = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.unsplash.com/search/photos`,
+        {
+          params: {
+            query: query,
+            client_id: "B9dWf0LUP-q7yo3vyC5Ivm4AdFxCJEC7xfmu4MdhxZY",
+          },
+        }
+      );
+
+      console.log(response.data.results[0].urls.small);
+      setImgUrl(response.data.results[0].urls.small);
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="card">
+          <h2 className="text-center">Image Genrator</h2>
+          <div className="flex">
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Search"
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
+            />
+            <button className="btn" onClick={imageHandler}>
+              Search
+            </button>
+          </div>
+
+          <div className="img-container">
+            <img src={imgUrl} alt="" />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
